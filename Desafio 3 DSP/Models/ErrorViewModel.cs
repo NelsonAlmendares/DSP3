cb5807356431a42c2a6b1324268b4c82fd6f9f49
NelsonAlmendares <Nelsonjosealmendares1999@gmail.com>
2024-10-23 13:53:46 -0600

    Inicio desafio 3 DSP

---
+++ Desafio 3 DSP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Desafio_3_DSP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
