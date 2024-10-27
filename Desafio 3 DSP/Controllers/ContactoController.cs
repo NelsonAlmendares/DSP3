1d2cd2d187934afec179172ae5464ef6ba0560ff
NelsonAlmendares <Nelsonjosealmendares1999@gmail.com>
2024-10-27 17:06:00 -0600

    Avace vistas y base

---
+++ Desafio 3 DSP/Controllers/ContactoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio_3_DSP.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Desafio 3 DSP/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio_3_DSP.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
